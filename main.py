24a3ab3cd768f9271afee816bd922498f06f5a81
MuhammadYasa <66125818+MuhammadYasa@users.noreply.github.com>
2020-06-17 09:49:55 +0700

    mendeskripsikan konstruksi dasar pada python, sequentials

---
+++ main.py
@@ -0,0 +1,7 @@
+# Contruction Python
+#sequentials : eksekusi sesuai urutan
+# semua eksekusi yang dilakukan sesuai urutan dari atas sampai kebawah
+print('Nama : Muhammad Yasa')
+print('Tempat lahir : Surabaya')
+print('Kebangsaan : Indonesia')
+print('Tanggal saat ini : 17 Juni 2020')
