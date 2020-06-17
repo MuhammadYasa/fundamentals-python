30c9ba699997475b9dca53966747a58f0f6549dd
MuhammadYasa <66125818+MuhammadYasa@users.noreply.github.com>
2020-06-17 10:00:34 +0700

    menambahkan deskripsi percabangan

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 # Contruction Python
-#sequentials : eksekusi sesuai urutan
+# sequentials : eksekusi sesuai urutan
 # semua eksekusi yang dilakukan sesuai urutan dari atas sampai kebawah
 print('Nama : Muhammad Yasa')
 print('Tempat lahir : Surabaya')
 print('Kebangsaan : Indonesia')
 print('Tanggal saat ini : 17 Juni 2020')
+print('/n')
+
+# percabangan : dengan syarat
+orang_sehat = True
+if orang_sehat:
+    print('olah raga & istirahat teratur')
+else:
+    print('makan junkfood dan bermalas malasan')
